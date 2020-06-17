fd1d2c2c17a0ae487e8db59fc9b828e84772b710
Chris Ducharme <ducharme.christophert@gmail.com>
2020-06-17 13:33:57 -0500

    error

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar"
 import ContentWrapper from "./components/ContentWrapper"
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 // Routes
 const routes = require("./routes");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 // database
