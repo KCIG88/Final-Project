5525d2f4d3765160f9165d0403a3001d633205c9
kevin cox <kevincox1983@gmail.com>
2020-06-24 13:40:33 -0500

    fix untracked

---
+++ client/src/components/MLBTable/index.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+
+import "../../styles/MyBetsTable.css";
+
+class MLBTable extends Component {
+
+  state = {
+    results: [],
+  };
+
+
+  componentDidMount() {
+    this.getMLBapi();
+  }
+
+
+  getMLBapi = (props) => {
+    API.getMLBoddsapi(props)
+      .then(res => this.setState({ results: res.data.data }))
+      .then(res => { console.log(res) })
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    const { results } = this.state
+    console.log(results)
+    return (
+
+      <>
+        <h1>MLB Games</h1>
+        <h1>No Results at this moment</h1>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Home Team</th>
+              <th>Away Team</th>
+              <th>Home Team Line</th>
+              <th>Home Team Win</th>
+              <th>Bet on this game</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {results.map(data => {
+              return (<tr>
+                <td>{data.commence_time}</td>
+              <td>{data.home_team}</td>
+              <td>{data.teams[0]}</td>
+              <td></td>
+              <td></td>
+              <td><button>Add Funds</button></td>
+
+              </tr>
+            )})} 
+           
+
+           </tbody>
+        </table>
+
+      </>
+
+    );
+  }
+}
+export default MLBTable;
---
+++ client/src/pages/NCAA.js
@@ -0,0 +1,22 @@
+import React from "react";
+import NCAATable from "../components/NCAATable"
+import MyBetsTable from "../components/MyBetsTable"
+import "../styles/Dashboard.css";
+
+
+function NCAA() {
+
+
+  return (
+  
+<>
+
+< NCAATable/>
+< MyBetsTable />
+
+</>
+     
+  );
+};
+
+export default NCAA;
