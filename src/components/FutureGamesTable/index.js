a88fc762975cdfbe40462702cc33140d7f9c78df
Chris Ducharme <58921765+ChrisDucharme1793@users.noreply.github.com>
2020-07-06 22:47:45 -0400

    Update index.js

--- src/components/FutureGamesTable/index.js
+++ src/components/FutureGamesTable/index.js
@@ -36,14 +36,14 @@ class FutureGamesTables extends Component {
 
     const {state, setState, history} = this.props
 
-    console.log(this.props.state)
-    const eventTime = results.map(data => {
-      return { ...data, commence_time: new Date(data.commence_time * 1000).toString() }
-    })
+    // console.log(this.props.state)
+    // const eventTime = results.map(data => {
+    //   return { ...data, commence_time: new Date(data.commence_time * 1000).toString() }
+    // })
 
-    console.log(eventTime)
+    // console.log(eventTime)
 
-    console.log(this.props)
+    // console.log(this.props)
 
     return (
 
@@ -65,13 +65,15 @@ class FutureGamesTables extends Component {
               </thead>
 
               <tbody>
-                {eventTime.map(data => {
+                {results.map(data => {
+                  console.log(data.sites[0])
+                  if(data.sites[0] !== undefined){
                   return (<tr>
                     <td >{data.commence_time}</td>
-                    <td>{data.home_team}</td>
+                    <td>{data.teams[0]}</td>
                     <td>{data.teams[1]}</td>
                     <td>{data.sites[0].odds.spreads.points[0]}</td>
-                    <td>{data.sites[0].odds.spreads.odds[0]}</td>
+                    <td></td>
 
                     <td> <button onClick={ () => { 
                       setState({...state, bet: data})
@@ -81,6 +83,8 @@ class FutureGamesTables extends Component {
                       }>Place a Bet</button></td>
                   </tr>
                   )
+                          
+                  }else {return ""}
                 })}
 
 
