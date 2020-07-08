5a1c171bde0b31b3899b66467d34ae5ffad6d430
Chris Ducharme <58921765+ChrisDucharme1793@users.noreply.github.com>
2020-07-08 14:53:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 
 
- const MONGODB_URI = 'mongodb+srv://auth0-custom-db-user:Hornet#94@cluster0-oidde.mongodb.net/project3?retryWrites=true&w=majority'
+ const MONGODB_URI = 'mongodb+srv://auth0-custom-db-user:Hornet94@cluster0-oidde.mongodb.net/project3?retryWrites=true&w=majority'
 
   mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/project3', {
     useNewUrlParser: true,
