fc15d6cf0d628060a3b6e614b771c5b80eee14ac
Chris Ducharme <58921765+ChrisDucharme1793@users.noreply.github.com>
2020-07-08 10:44:31 -0500

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -87,7 +87,7 @@ class Navbar extends Component {
     return (
 
       <div>
-        <nav className="navbar" role="navigation" aria-label="main navigation">
+        <nav className="navbar is-transparent" role="navigation" aria-label="main navigation">
           <div className="navbar-brand-is-dark">
 
 
